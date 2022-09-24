8b80c434d51741cf756269989ce510ba8917efc2
panda1 <blueblue0011@gmail.com>
2022-09-24 09:00:30 -0700

    found a better way for this and it works

--- app.py
+++ app.py
@@ -1,32 +1,47 @@
 thoughts = []
 
-def your_thoughts():
-    while True:
-        try:
-            selection = int(input('choose 1, 2, or 3: '))
-            if(selection > 0 or selection < 4):
-                while True:
-                    if(selection == 1):
-                        your_thought = input("your thoughts will be stored: ")
-                        thoughts.append(your_thought)
-                        if(thoughts != None):
-                            ask_user = input("another thought?(y or n) ")
-                            if(ask_user == 'n'):
-                                break
+# def your_thoughts():
+#     while True:
+#         try:
+#             selection = int(input('choose 1, 2, or 3: '))
+#             if(selection > 0 or selection < 4):
+#                 while True:
+#                     if(selection == 1):
+#                         your_thought = input("your thoughts will be stored: ")
+#                         thoughts.append(your_thought)
+#                         if(thoughts != None):
+#                             ask_user = input("another thought?(y or n) ")
+#                             if(ask_user == 'n'):
+#                                 break
                         
-                    elif(selection == 2):
-                        print('this is your thoughts so far: ')
-                        read_thoughts(thoughts)
-                    elif(selection == 3):
-                        print('good bye for now')
-                        break
-                break
-            else:
-                selection = 'error'
-                print('you didnt choose within the range of number, so try again')
-        except ValueError:
-            print("you're a creative one, try again")
-    return selection
+#                     elif(selection == 2):
+#                         print('this is your thoughts so far: ')
+#                         read_thoughts(thoughts)
+#                     elif(selection == 3):
+#                         print('good bye for now')
+#                         break
+#                 break
+#             else:
+#                 selection = 'error'
+#                 print('you didnt choose within the range of number, so try again')
+#         except ValueError:
+#             print("you're a creative one, try again")
+#     return selection
+
+
+def your_thoughts():
+    
+        selection = input('choose 1, 2, or 3: ')
+        if selection in ('1'):
+            record_thought = input('your thoughts please: ')
+            thoughts.append(record_thought)
+            your_thoughts()
+        elif selection in ('2'):
+            read_thoughts(thoughts)
+            your_thoughts()
+        elif selection in ('3'):
+            print('good bye for now')
+   
 
 
 
